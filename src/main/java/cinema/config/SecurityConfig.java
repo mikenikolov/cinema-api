04cdd7b9c805e1ab990758c13730b7fc2b906060
Mykhailo Nikolov <mikenikolov.work@gmail.com>
2022-09-16 23:08:54 +0300

    style: code style for antMatchers

--- src/main/java/cinema/config/SecurityConfig.java
+++ src/main/java/cinema/config/SecurityConfig.java
@@ -23,18 +23,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .authorizeRequests()
                 .antMatchers("/register", "/login").permitAll()
-                .antMatchers(HttpMethod.POST, "/cinema-halls", "/movies",
-                        "/movie-sessions").hasRole("ADMIN")
+                .antMatchers(HttpMethod.POST, "/cinema-halls", "/movies", "/movie-sessions")
+                    .hasRole("ADMIN")
                 .antMatchers(HttpMethod.POST, "/orders/complete").hasRole("USER")
-                .antMatchers(HttpMethod.GET, "/cinema-halls", "/movies",
-                        "/movie-sessions").hasAnyRole("ADMIN", "USER")
-                .antMatchers(HttpMethod.GET, "/orders",
-                        "/shopping-carts/by-user").hasRole("USER")
+                .antMatchers(HttpMethod.GET, "/cinema-halls", "/movies", "/movie-sessions")
+                    .hasAnyRole("ADMIN", "USER")
+                .antMatchers(HttpMethod.GET, "/orders", "/shopping-carts/by-user").hasRole("USER")
                 .antMatchers(HttpMethod.GET,"/users/by-email").hasRole("ADMIN")
                 .antMatchers(HttpMethod.PUT, "/movie-sessions/{id}").hasRole("ADMIN")
                 .antMatchers(HttpMethod.DELETE, "/movie-sessions/{id}").hasRole("ADMIN")
-                .antMatchers(HttpMethod.PUT,
-                        "/shopping-carts/movie-sessions").hasRole("USER")
+                .antMatchers(HttpMethod.PUT, "/shopping-carts/movie-sessions").hasRole("USER")
                 .anyRequest().authenticated()
                 .and()
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
